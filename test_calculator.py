9eb503cd7a34138107b0b4a00bba1ffde58d44f7
Estelle <estelleclement@MacBook-Air-de-Estelle.local>
2024-04-12 15:21:52 +0200

    Add test_substraction and test_division

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,12 @@
 from calculator import addition
+from calculator import substraction
+from calculator import division
 
 def test_addition():
     assert addition(1, 2) == 3
+
+def test_substraction():
+    assert substraction(3, 2) == 1
+
+def test_division():
+    assert division(6, 2) == 3
